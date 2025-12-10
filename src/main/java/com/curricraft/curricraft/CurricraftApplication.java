e1502e1a9fdb4545883a92650ea609d64538198a
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-10 11:24:30 +0900

    feat: initial commit

---
+++ src/main/java/com/curricraft/curricraft/CurricraftApplication.java
@@ -0,0 +1,13 @@
+package com.curricraft.curricraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurricraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurricraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curricraft/curricraft/CurricraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curricraft.curricraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurricraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
