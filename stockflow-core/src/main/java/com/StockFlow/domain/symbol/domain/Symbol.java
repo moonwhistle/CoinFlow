d8f6ce076ad862183f1ef2396c79bed5c049b348
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-12 14:52:23 +0900

    docs: 제한사항 및 선택 기준 기록

--- stockflow-core/src/main/java/com/StockFlow/domain/symbol/domain/Symbol.java
+++ stockflow-core/src/main/java/com/StockFlow/domain/symbol/domain/Symbol.java
@@ -50,7 +50,7 @@ public class Symbol {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
     @PrePersist
