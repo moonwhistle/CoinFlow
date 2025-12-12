73a56048c3187ab0f8ef841c88f9f637da6ae967
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-12 14:05:57 +0900

    feat: ticker 엔티티 필요 없음에 따라 Tick 엔티티로 변경

--- stockflow-core/src/main/java/com/StockFlow/domain/tick/domain/Tick.java
+++ stockflow-core/src/main/java/com/StockFlow/domain/tick/domain/Tick.java
@@ -1,8 +1,6 @@
-package com.StockFlow.domain.ticker.domain;
+package com.StockFlow.domain.tick.domain;
 
 import com.StockFlow.domain.symbol.domain.Symbol;
-import com.StockFlow.domain.ticker.exception.TickerErrorCode;
-import com.StockFlow.domain.ticker.exception.TickerException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,7 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -26,51 +23,30 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-public class Ticker {
+public class Tick {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "symbol_id", nullable = false)
     private Symbol symbol;
 
     @Column(nullable = false, precision = 18, scale = 6)
-    private BigDecimal lastPrice;
+    private BigDecimal price;
 
-    @Column(precision = 18, scale = 6)
-    private BigDecimal change; // 가격 변화량
-
-    @Column(precision = 18, scale = 6)
-    private BigDecimal changePercent; // 변동률 %
-
-    @Column
+    @Column(nullable = false)
     private Long volume;
 
-    // 실제 시장 시간
     @Column(nullable = false)
-    private LocalDateTime timestamp;
+    private LocalDateTime tickTime;
 
-    // DB 저장 시간 (서버 기준)
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column
-    private LocalDateTime updatedAt;
+    private LocalDateTime receivedAt;
 
     @PrePersist
     public void prePersist() {
-        if(timestamp == null) {
-            throw new TickerException(TickerErrorCode.TIMESTAMP_REQUIRED);
-        }
-
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = this.createdAt;
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
+        this.receivedAt = LocalDateTime.now();
     }
 }
