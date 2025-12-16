5298be2f9410239031228dfd97b2a0a8eabe3f70
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-16 13:41:27 +0900

    feat: consumerConfig 구현

--- coinflow-consumer-app/src/main/java/com/coinflow/ConsumerApplication.java
+++ coinflow-consumer-app/src/main/java/com/coinflow/ConsumerApplication.java
@@ -1,15 +1,12 @@
 package com.coinflow;
 
-import com.coinflow.config.properties.TickConsumerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(
         exclude = DataSourceAutoConfiguration.class
 )
-@EnableConfigurationProperties(TickConsumerProperties.class)
 public class ConsumerApplication {
 
     public static void main(String[] args) {
