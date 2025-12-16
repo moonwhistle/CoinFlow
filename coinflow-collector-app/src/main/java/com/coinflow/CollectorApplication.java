8933bbf2ecdb9a8c271e188554eb60d2b09916a0
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-16 13:55:25 +0900

    feat: redis consumer group initializer 구현

--- coinflow-collector-app/src/main/java/com/coinflow/CollectorApplication.java
+++ coinflow-collector-app/src/main/java/com/coinflow/CollectorApplication.java
@@ -1,15 +1,12 @@
 package com.coinflow;
 
-import com.coinflow.config.properties.BinanceWebSocketProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(
         exclude = DataSourceAutoConfiguration.class
 )
-@EnableConfigurationProperties(BinanceWebSocketProperties.class)
 public class CollectorApplication {
 
     public static void main(String[] args) {
