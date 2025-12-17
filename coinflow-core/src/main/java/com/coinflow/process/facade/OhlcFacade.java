18fce4088e3166e830ebc383d429c59ec7ca42ea
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-17 16:02:54 +0900

    feat: DB I/O 감소를 위해 메모리에 데이터를 저장해주는 기능 구현

--- coinflow-core/src/main/java/com/coinflow/process/facade/OhlcFacade.java
+++ coinflow-core/src/main/java/com/coinflow/process/facade/OhlcFacade.java
@@ -3,7 +3,7 @@ package com.coinflow.process.facade;
 import com.coinflow.domain.symbol.domain.Symbol;
 import com.coinflow.domain.symbol.service.SymbolService;
 import com.coinflow.domain.tick.event.TickRawEvent;
-import com.coinflow.process.service.Ohlc1mUpdateService;
+import com.coinflow.process.store.Ohlc1mAggregationStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OhlcFacade {
 
-    private final Ohlc1mUpdateService ohlc1mUpdateService;
+    private final Ohlc1mAggregationStore ohlc1MAggregationStore;
     private final SymbolService symbolService;
 
     public void process(TickRawEvent event) {
         Symbol symbol = symbolService.findBySymbol(event.symbol());
-        ohlc1mUpdateService.update(symbol, event);
+        ohlc1MAggregationStore.accumulate(symbol, event);
     }
 }
