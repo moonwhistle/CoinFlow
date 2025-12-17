e356aeb8d410416e42da187fbb7510abe6131004
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-17 13:05:03 +0900

    feat: ohlc 테이블 변환 로직을 위한 db 세팅 및 create table DDL 작성

--- coinflow-core/src/main/java/com/coinflow/domain/ohlc/domain/Ohlc1m.java
+++ coinflow-core/src/main/java/com/coinflow/domain/ohlc/domain/Ohlc1m.java
@@ -2,12 +2,14 @@ package com.coinflow.domain.ohlc.domain;
 
 import com.coinflow.domain.symbol.domain.Symbol;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "ohlc_1m")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Ohlc1m extends AbstractOhlc {
 
