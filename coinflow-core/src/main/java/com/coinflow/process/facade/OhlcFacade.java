0aae555ff65c8055fcf863abf17d34a553c34ea4
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-17 14:38:13 +0900

    feat: symbol 조회 기능 구현 및 facade 클래스 뼈대 구현

--- coinflow-core/src/main/java/com/coinflow/process/facade/OhlcFacade.java
+++ coinflow-core/src/main/java/com/coinflow/process/facade/OhlcFacade.java
@@ -1,12 +1,21 @@
 package com.coinflow.process.facade;
 
+import com.coinflow.domain.symbol.domain.Symbol;
+import com.coinflow.domain.symbol.service.SymbolService;
 import com.coinflow.domain.tick.event.TickRawEvent;
-import org.springframework.stereotype.Component;
+import com.coinflow.process.service.Ohlc1mUpdateService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
+@RequiredArgsConstructor
 public class OhlcFacade {
 
+    private final Ohlc1mUpdateService ohlc1mUpdateService;
+    private final SymbolService symbolService;
+
     public void process(TickRawEvent event) {
-        // TODO: saveTick
+        Symbol symbol = symbolService.findBySymbol(event.symbol());
+        ohlc1mUpdateService.update(symbol, event);
     }
 }
