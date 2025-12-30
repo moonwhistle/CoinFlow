573b28645e9de9affbbf481ce0fecce0407b00cb
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2025-12-30 16:10:24 +0900

    feat: 캐시 저장소 구현

--- coinflow-core/src/main/java/com/coinflow/domain/ohlc/cache/OhlcChartStore.java
+++ coinflow-core/src/main/java/com/coinflow/domain/ohlc/cache/OhlcChartStore.java
@@ -4,12 +4,20 @@ import com.coinflow.domain.ohlc.constant.OhlcInterval;
 import com.coinflow.domain.ohlc.snapshot.OhlcCandleSnapshot;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface OhlcChartStore {
 
-    List<OhlcCandleSnapshot> loadRecent(
+    Optional<List<OhlcCandleSnapshot>> get(
             Long symbolId,
             OhlcInterval interval,
             LocalDateTime endExclusive
+    );
+
+    void put(
+            Long symbolId,
+            OhlcInterval interval,
+            LocalDateTime endExclusive,
+            List<OhlcCandleSnapshot> candles
     );
 }
