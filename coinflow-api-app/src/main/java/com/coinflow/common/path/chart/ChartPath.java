7b302cc26bc35f28dbd405683f287d3132c23927
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-01-07 14:45:26 +0900

    feat: api 경로 interval 과 candle 범위를 파라미터로 받도록 변경

--- coinflow-api-app/src/main/java/com/coinflow/common/path/chart/ChartPath.java
+++ coinflow-api-app/src/main/java/com/coinflow/common/path/chart/ChartPath.java
@@ -2,5 +2,8 @@ package com.coinflow.common.path.chart;
 
 public class ChartPath {
 
-    public static final String SHOW_OHLC = "/api/v1/ohlc/{interval}/{symbolId}";
+    /**
+     * GET /api/v1/ohlc/{symbolId}?interval=1m&candles=120
+     */
+    public static final String OHLC = "/api/v1/ohlc/{symbolId}";
 }
