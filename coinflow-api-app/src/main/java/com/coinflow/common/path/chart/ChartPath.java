46e576bcc8b4ea44f706267710b277536b0525d3
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-01-07 13:30:00 +0900

    feat: api 경로 수정

--- coinflow-api-app/src/main/java/com/coinflow/common/path/chart/ChartPath.java
+++ coinflow-api-app/src/main/java/com/coinflow/common/path/chart/ChartPath.java
@@ -2,5 +2,5 @@ package com.coinflow.common.path.chart;
 
 public class ChartPath {
 
-    public static final String OHLC_1M = "/api/v1/ohlc/1m/{symbolId}";
+    public static final String SHOW_OHLC = "/api/v1/ohlc/{interval}/{symbolId}";
 }
