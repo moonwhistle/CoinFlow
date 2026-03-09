75c405ddf4e56679e422300d10f673fd72438627
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-03-09 22:24:42 +0900

    feat: Integrate `ReplayErrorCode` with `BaseErrorCode` and `ReplayException` with `BaseException` for standardized exception handling.

--- backend/coinflow-replay-app/src/main/java/com/coinflow/replay/common/exception/ReplayException.java
+++ backend/coinflow-replay-app/src/main/java/com/coinflow/replay/common/exception/ReplayException.java
@@ -1,2 +1,11 @@
-package com.coinflow.replay.common.exception;public class ReplayException {
+package com.coinflow.replay.common.exception;
+
+import com.coinflow.exception.BaseErrorCode;
+import com.coinflow.exception.BaseException;
+
+public class ReplayException extends BaseException {
+
+    public ReplayException(BaseErrorCode errorCode) {
+        super(errorCode);
+    }
 }
