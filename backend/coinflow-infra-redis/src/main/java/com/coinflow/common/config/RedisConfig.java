663407c91cc73177f2691fb17b3f8c5c9b302eb9
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2026-03-27 11:09:39 +0900

    feat: binary 필드 redistemplate 추가

--- backend/coinflow-infra-redis/src/main/java/com/coinflow/common/config/RedisConfig.java
+++ backend/coinflow-infra-redis/src/main/java/com/coinflow/common/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -23,6 +24,24 @@ public class RedisConfig {
 
         template.setHashKeySerializer(stringSerializer);
         template.setHashValueSerializer(stringSerializer);
+
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, byte[]> rawRedisTemplate(
+            RedisConnectionFactory factory
+    ) {
+        RedisTemplate<String, byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(RedisSerializer.byteArray());
+
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(RedisSerializer.byteArray());
 
         return template;
     }
